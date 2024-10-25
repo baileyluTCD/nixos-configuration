4801172d46f770b98e84cfd2c455b6c3c45901f5
Luke Bailey <baileylu@tcd.ie>
2024-10-25 13:53:27 +0100

    added nix build toolchain

--- configuration.nix
+++ configuration.nix
@@ -1,9 +1,9 @@
 # Edit this configuration file to define what should be installed on your system. Help is available in the configuration.nix(5) man page, on https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
 { config, lib, pkgs, ... }:
-
-{ imports =
-  [ 
+  
+{
+  imports = [ 
     # Include the results of the hardware scan.
     ./hardware-configuration.nix 
   ];
@@ -65,6 +65,8 @@
     lshw
     kitty
     clang
+    nil
+    alejandra 
   ];
 
   programs.hyprland.enable = true;
--- flake.nix
+++ flake.nix
@@ -3,12 +3,20 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    
+    alejandra.url = "github:kamadorueda/alejandra/3.0.0";
+    alejandra.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, alejandra }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [ ./configuration.nix ];
+      modules = [ 
+          {
+            environment.systemPackages = [alejandra.defaultPackage.x86_64-linux];
+          }
+        ./configuration.nix
+      ];
     }; 
   };
 }
