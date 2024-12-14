4dfead7da3ff82a16512e218c659dbe9b243cce8
Luke Bailey <baileylu@tcd.ie>
2024-12-14 22:24:49 +0000

    added users configuration file

---
+++ system/users.nix
@@ -0,0 +1,12 @@
+let
+  username = "luke";
+in {
+  # Trust myself to modify nix settings
+  nix.settings.trusted-users = [username];
+
+  # Define my personal user account so I don't have to login as root
+  users.users.${username} = {
+    isNormalUser = true;
+    extraGroups = ["wheel"]; # Enable ‘sudo’ for the user.
+  };
+}
