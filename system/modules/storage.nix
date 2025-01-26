a1286223d71d129aba2ba7c8b68df91f2f2dd198
Luke Bailey <baileylu@tcd.ie>
2025-01-26 23:54:13 +0000

    setup second ssd mount as home

--- system/modules/storage.nix
+++ system/modules/storage.nix
@@ -1,4 +1,10 @@
 {...}: {
+  # Mount second ssd to home
+  fileSystems."/home" = {
+    device = "/dev/sda";
+    fsType = "ext4";
+  };
+
   # Create a swap file the same size as system ram so hibernation can work
   swapDevices = [
     {
