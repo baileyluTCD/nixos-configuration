ee7751a5cba401031b8769fc17f7752b4c649bf8
Luke Bailey <baileylu@tcd.ie>
2025-02-03 17:03:25 +0000

    setup hyprland boot through uwsm as recommended

--- system/modules/startup.nix
+++ system/modules/startup.nix
@@ -9,24 +9,6 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # Manage enviroenvironment variables
-  environment.variables = {
-    # Enable wayland support for electron apps
-    NIXOS_OZONE_WL = "1";
-  };
-
-  # Boot into hyprland on startup
-  services.greetd = {
-    enable = true;
-    settings = rec {
-      initial_session = {
-        command = "${hyprland-configured}/bin/Hyprland";
-        user = config.users.primary;
-      };
-      default_session = initial_session;
-    };
-  };
-
   # Enable usage of hyprlock lock screen through pam
   # https://en.wikipedia.org/wiki/Linux_PAM
   security.pam.services.hyprlock = {};
@@ -34,8 +16,18 @@
   # Use zsh as the default shell system wide over bash
   users.defaultUserShell = zsh-configured;
 
-  # Enable unconfigured hyprland as minimum for each user
-  programs.hyprland.enable = true;
+  # Boot into hyprland on startup
+  services.greetd = {
+    enable = true;
+    settings = rec {
+      initial_session = {
+        command = "${pkgs.uwsm}/bin/uwsm start ${hyprland-configured}/bin/Hyprland";
+        user = config.users.primary;
+      };
+      default_session = initial_session;
+    };
+  };
+
   environment.systemPackages = with pkgs; [
     # Add the lockscreen package
     hyprlock
