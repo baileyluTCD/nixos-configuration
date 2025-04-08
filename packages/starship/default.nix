f82a4e32db6bba1384048faac49e0f90c039b915
Luke Bailey <baileylu@tcd.ie>
2025-04-08 14:41:19 +0100

    Refactored starship wrapper package with writeShellApplication

--- packages/starship/default.nix
+++ packages/starship/default.nix
@@ -1,26 +1,9 @@
-{
-  pkgs,
-  pname,
-  flake,
-  ...
-}:
-pkgs.stdenv.mkDerivation {
-  inherit pname;
-  version = "1.0.0";
-
-  src = ./starship.toml;
-
-  unpackPhase = ":";
-
-  # Inputs for wrapping program
-  nativeBuildInputs = with pkgs; [
-    makeWrapper
-  ];
-
-  buildPhase = ''
-    mkdir -p $out/bin
-
-    makeWrapper "${pkgs.starship}/bin/starship" $out/bin/${pname} \
-      --set STARSHIP_CONFIG $src
+{pkgs, ...}:
+pkgs.writeShellApplication {
+  name = "starship";
+  runtimeInputs = [pkgs.starship];
+  runtimeEnv.STARSHIP_CONFIG = ./starship.toml;
+  text = ''
+    exec starship "$@"
   '';
 }
