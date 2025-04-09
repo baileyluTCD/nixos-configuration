4cad2d5918a52f54f305293967bd7938f6c89eb7
Luke Bailey <baileylu@tcd.ie>
2025-04-09 12:29:16 +0100

    Refactored neovim package

--- packages/neovim/default.nix
+++ packages/neovim/default.nix
@@ -1,123 +1,42 @@
 {
   pkgs,
   pname,
-  flake,
   ...
 }: let
-  lib = pkgs.lib;
-
-  # Packages needed for running various functions (LSPs, etc)
-  packages = with pkgs; [
-    # Tools used by config
-    ripgrep
-    fd
-    nodejs_22
-    zoxide
-    direnv
-    tree-sitter
-
-    # Extra formatters
-    alejandra
-  ];
-
   # Nvim Plugins to make avalible
-  plugins = with pkgs.vimPlugins; [
-    # Lazy load nvim plugins
-    lz-n
-
-    # Colorscheme
-    gruvbox-nvim
-    transparent-nvim
-
-    # Fuzzy finder ui
-    telescope-nvim
-    telescope-zoxide
-    telescope-fzf-native-nvim
-    telescope-ui-select-nvim
-    nvim-web-devicons
-
-    # Status line
-    lualine-nvim
-
-    # File explorer
-    oil-nvim
-
-    # Quick file swapping
-    harpoon2
-
-    # Git modification signs
-    gitsigns-nvim
-
-    # Provides keybind hints
-    which-key-nvim
-
-    # Convenience functions
-    mini-nvim
-    plenary-nvim
-
-    # Language servers
-    nvim-lspconfig
-    fidget-nvim
-
-    # Syntax Highlighting
-    nvim-treesitter
-    todo-comments-nvim
-
-    # Autocomplete
-    nvim-cmp
-    luasnip
-    cmp_luasnip
-    cmp-nvim-lsp
-    cmp-path
-
-    # Code formatting
-    conform-nvim
-
-    # Smart indentation
-    vim-sleuth
-
-    # Start screen
-    startup-nvim
-
-    # Portable environment setup
-    lazy-lsp-nvim
-    direnv-vim
-  ];
+  plugins = pkgs.callPackage ./plugins.nix {vimPlugins = pkgs.vimPlugins;};
 
   # Produce a valid vim packpath from the plugins list
   packpath = pkgs.runCommandLocal "packpath" {} ''
     mkdir -p $out/pack/${pname}/{start,opt}
 
     ${
-      lib.concatMapStringsSep
+      pkgs.lib.concatMapStringsSep
       "\n"
-      (plugin: "ln -vsfT ${plugin} $out/pack/${pname}/start/${lib.getName plugin}")
+      (plugin: "ln -vsfT ${plugin} $out/pack/${pname}/start/${pkgs.lib.getName plugin}")
       plugins
     }
   '';
 in
-  pkgs.stdenv.mkDerivation {
-    inherit pname;
-    version = "1.0.0";
+  pkgs.writeShellApplication {
+    name = "nvim";
 
-    src = ./src;
+    runtimeInputs = with pkgs; [
+      neovim-unwrapped
 
-    nativeBuildInputs = with pkgs; [
-      makeWrapper
+      # Tools used by config
+      ripgrep
+      fd
+      nodejs_22
+      zoxide
+      direnv
+      tree-sitter
     ];
 
-    buildPhase = ''
-      export NVIM_DIR=$out/bin
-      mkdir -p $out/bin
-
-      makeWrapper "${pkgs.neovim-unwrapped}/bin/nvim" $out/bin/nvim \
-        --add-flags "--cmd 'set packpath^=${packpath} | set rtp^=${packpath}'" \
-        --add-flags "--cmd 'set rtp^=$out/bin'" \
-        --add-flags "-u '$out/bin/init.lua'" \
-        --prefix PATH : ${lib.makeBinPath packages}
-    '';
-
-    installPhase = ''
-      cp -r $src/* $out/bin
+    text = ''
+      exec nvim "$@" \
+        --cmd 'set packpath^=${packpath} | set rtp^=${packpath}' \
+        --cmd 'set rtp^=${./src}' \
+        -u '${./src/init.lua}'
     '';
   }
---
+++ packages/neovim/plugins.nix
@@ -0,0 +1,63 @@
+{vimPlugins, ...}:
+with vimPlugins; [
+  # Lazy load nvim plugins
+  lz-n
+
+  # Colorscheme
+  gruvbox-nvim
+  transparent-nvim
+
+  # Fuzzy finder ui
+  telescope-nvim
+  telescope-zoxide
+  telescope-fzf-native-nvim
+  telescope-ui-select-nvim
+  nvim-web-devicons
+
+  # Status line
+  lualine-nvim
+
+  # File explorer
+  oil-nvim
+
+  # Quick file swapping
+  harpoon2
+
+  # Git modification signs
+  gitsigns-nvim
+
+  # Provides keybind hints
+  which-key-nvim
+
+  # Convenience functions
+  mini-nvim
+  plenary-nvim
+
+  # Language servers
+  nvim-lspconfig
+  fidget-nvim
+
+  # Syntax Highlighting
+  nvim-treesitter
+  todo-comments-nvim
+
+  # Autocomplete
+  nvim-cmp
+  luasnip
+  cmp_luasnip
+  cmp-nvim-lsp
+  cmp-path
+
+  # Code formatting
+  conform-nvim
+
+  # Smart indentation
+  vim-sleuth
+
+  # Start screen
+  startup-nvim
+
+  # Portable environment setup
+  lazy-lsp-nvim
+  direnv-vim
+]
