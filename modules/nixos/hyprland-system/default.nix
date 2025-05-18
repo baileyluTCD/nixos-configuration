e9d3274705088068fa4a7138db4cb0272b1b1c63
Luke Bailey <baileylu@tcd.ie>
2025-05-18 13:57:06 +0100

    Enable better application compat

--- modules/nixos/hyprland-system/default.nix
+++ modules/nixos/hyprland-system/default.nix
@@ -11,10 +11,13 @@
     ./printing.nix
     ./secrets.nix
     ./startup.nix
+    ./virtualisation.nix
   ];
 
   # Enable flakes
   nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  programs.nix-ld.enable = true;
 
   # List packages installed in system profile. To search, run: $ nix search nixpkgs wget
   environment.systemPackages = with pkgs; [
