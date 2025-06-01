17e111f65035ed328736600b6bd6f9f8b6bd41c1
Luke Bailey <baileylu@tcd.ie>
2025-06-01 13:01:38 +0100

    Setup niri login and desktop bg

--- modules/nixos/niri-system/default.nix
+++ modules/nixos/niri-system/default.nix
@@ -1,7 +1,48 @@
 {
   pkgs,
-  flake,
   ...
-}: {
+}: 
+ let config-background-daemon = pkgs.writeShellApplication {
+    name = "config-background-daemon";
+    runtimeInputs = with pkgs; [
+      swaybg
+      swww
+    ];
+    text = ''
+      swaybg -i ${./desktopBackgroundBlurred.png} &
+
+      swww-daemon &
+      swww img ${./desktopBackground.png}
+    '';
+ };
+ in
+{
   programs.niri.enable = true;
+
+  programs.regreet = {
+    theme = {
+      name = "Nordic-darker";
+      package = pkgs.nordic;
+    };
+    iconTheme = {
+      name = "Nordzy-dark";
+      package = pkgs.nordzy-icon-theme;
+    };
+    cursorTheme = {
+      name = "Nordzy-cursors";
+      package = pkgs.nordzy-cursor-theme;
+    };
+    font = {
+      name = "Inter";
+      package = pkgs.inter;
+    };
+    settings.background.path = ./desktopBackground.png;
+    enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    wl-clipboard
+    sysmenu
+    config-background-daemon
+  ];
 }
--- modules/nixos/user-luke.nix
+++ modules/nixos/user-luke.nix
@@ -6,15 +6,5 @@
     extraGroups = ["wheel" "networkmanager"];
   };
 
-  services.greetd = {
-    enable = true;
-    settings = {
-      default_session = {
-        command = "${pkgs.niri}/bin/niri";
-        user = "luke";
-      };
-    };
-  };
-
   nix.settings.trusted-users = [ "luke" "root" ];
 }
